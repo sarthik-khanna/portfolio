54f1eafe1d0ff7807bd2db4289b888e66c6eb234
khanna-sarthik <sarthikkhanna28@gmail.com>
2025-07-06 23:35:23 +0530

    Remove commented-out "Services" link from Navbar component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -37,7 +37,6 @@ setIsScroll(false);
      <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : " bg-white shadow-sm bg-opacity-50"}`}>
          <li><a href="#top" className='font-Ovo'>Home</a></li>   
       <li><a className='font-Ovo' href="#about">About Me</a></li>
-        {/* <li><a className='font-Ovo' href="#services">Services</a></li>  */}
         <li><a className='font-Ovo' href="#work">My Work</a></li> 
           <li><a className='font-Ovo' href="#contact">Contact Me</a></li>  
      </ul>
